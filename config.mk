410892ff3c607b2568d22cb4c217a0d9d538bf97
Akos Asztalos <akos.asztalos@gmail.com>
2015-08-26 13:50:51 +0200

    Prefix config modified

--- config.mk
+++ config.mk
@@ -1,10 +1,10 @@
 # dwm version
-VERSION = 6.0
+VERSION = 6.0-modified
 
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = ~/.local
 MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
