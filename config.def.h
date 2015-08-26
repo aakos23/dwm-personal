f06dcc6068dfab5f851067e3894a67f008ac8988
Akos Asztalos <akos.asztalos@gmail.com>
2015-08-26 15:31:38 +0200

    Apply centred floating patch

--- config.def.h
+++ config.def.h
@@ -15,9 +15,10 @@ static const Bool topbar            = True;     /* False means bottom bar */
 
 
 static const Rule rules[] = {
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	/* class      instance    title       tags mask     isfloating   iscentred    monitor */
+	{ "Gimp",     NULL,       NULL,       0,            True,        False,       -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       False,       -1 },
+	{ "mpv",      NULL,       NULL,       0,            True,        True,        -1 },
 };
 
 /* layout(s) */
